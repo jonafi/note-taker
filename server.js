91f9857cb269e29354e8b024a5b843219d578054
J Ferro <jonafi@gmail.com>
2020-06-10 11:28:29 -0500

    fixed folder naming issue

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var notes = [
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 
