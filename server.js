b79af0a19b798c0cdacea871ea173f56932e9d0d
unknown <jonafi@gmail.com>
2020-06-11 17:01:31 -0500

    cleaned up code added route for unique ID

--- server.js
+++ server.js
@@ -27,7 +27,6 @@ app.get("/api/notes", function(req, res) {
 app.post("/api/notes", function(req, res) {
   let newNote = JSON.stringify(req.body);  
   let dbLocation = path.join(__dirname, "db/db.json");
-  //console.log(dbLocation);
   fs.readFile(dbLocation, 'utf8', function(err,data){
     if(err) throw err;
     console.log(data);
@@ -58,42 +57,13 @@ app.post("/api/notes", function(req, res) {
 });
 
 
-// // Displays all characters
-// app.get("/api/waitlist", function(req, res) {
-//   return res.json(waitlist);
-// });
+// Display a single note
+app.get("/api/notes/:id", function(req, res) {
+  let noteIndex = req.params.id;
+  console.log(noteIndex)
+});
 
-// // Displays a single character, or returns false
-// app.get("/api/waitlist/:customer", function(req, res) {
-//   let chosen = req.params.character;
 
-//   console.log(chosen);
-
-//   for (let i = 0; i < waitlist.length; i++) {
-//     if (chosen === waitlist[i].routeName) {
-//       return res.json(waitlist[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// // Create New Characters - takes in JSON input
-// app.post("/api/waitlist", function(req, res) {
-//   // req.body hosts is equal to the JSON post sent from the user
-//   // This works because of our body parsing middleware
-//   let newCustomer = req.body;
-
-//   // Using a RegEx Pattern to remove spaces from newCharacter
-//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-//   newCustomer.routeName = newCustomer.name.replace(/\s+/g, "").toLowerCase();
-
-//   console.log(newCustomer);
-
-//   characters.push(newCustomer);
-
-//   res.json(newCustomer);
-// });
 
 // Server start and console message //////////////////////////////
  
